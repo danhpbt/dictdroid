b03f61248070773f4af017de497ccc701ed41cd7
Danh Phan <pbtdanh@yahoo.com>
2020-03-12 21:32:03 -0700

    + add test

--- app/src/main/java/com/xynotec/dictdroid/data/model/History.java
+++ app/src/main/java/com/xynotec/dictdroid/data/model/History.java
@@ -8,10 +8,28 @@ import io.realm.annotations.Required;
 public class History extends RealmObject {
     @PrimaryKey
     @Required //not null
+    private String compoundKey;
+
+    private int dictLang;
+
+    @Required //not null
     private String word;
 
     @Required //not null
     private String mean;
+
+    public History()
+    {
+
+    }
+
+    public void setWordMean(String word, String mean, int dictLang)
+    {
+        this.dictLang = dictLang;
+        this.word = word;
+        this.mean = mean;
+        this.compoundKey = String.format("%d_%s", dictLang, word);
+    }
 
     public String getWord() {
         return word;
