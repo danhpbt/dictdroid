3960ea41415a1e8737ea9b8fa38a26db742c5490
Danh Phan <pbtdanh@yahoo.com>
2020-03-15 19:30:25 -0700

    + add test

---
+++ app/src/main/java/com/xynotec/dictdroid/ui/login/LoginViewModel.java
@@ -0,0 +1,37 @@
+package com.xynotec.dictdroid.ui.login;
+
+import androidx.databinding.ObservableField;
+
+import com.xynotec.dagger.BaseViewModel;
+import com.xynotec.dictdroid.data.DataManager;
+
+public class LoginViewModel extends BaseViewModel {
+
+    public final ObservableField<String> mUserName = new ObservableField<>();
+    public final ObservableField<String> mPassWord = new ObservableField<>();
+    
+    public LoginViewModel(DataManager dataManager) {
+        super(dataManager);
+    }
+
+    public ObservableField<String> getUserName()
+    {
+        return mUserName;
+    }
+
+    public ObservableField<String> getPassWord()
+    {
+        return mPassWord;
+    }
+
+    public void setUserName(String userName)
+    {
+        mUserName.set(userName);
+    }
+
+    public void setPassWord(String passWord)
+    {
+        mPassWord.set(passWord);
+    }
+
+}
